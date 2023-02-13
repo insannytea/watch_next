2e9551673dddea3cbc46a68b2cf6eca86d00683a
insannytea <saulenas1232@gmail.com>
2023-02-13 13:13:12 +0000

    simplify code

--- watch_next.py
+++ watch_next.py
@@ -2,20 +2,17 @@
 '''
 This program helps decide on what to 'watch later' with help from natural language generation
 '''
-
 #---Import section---
 import spacy
 nlp = spacy.load('en_core_web_md')
 
 # movie suggesting function
-def suggested_movie(a_movie):
+def suggested_movie(a_movie_desc):
     '''
     Takes in movie name and description as one string, separates into name and description,
     compares input movie description to each movie description from data base.
     Outputs movie with highest similarity to input movie as suggested movie
     '''
-    a_movie_split = a_movie.split(" :")
-    a_movie_desc = a_movie_split[1]
     a_movie_desc = nlp(a_movie_desc)
     highest_similarity_coefficient = 0
 
@@ -38,9 +35,9 @@ movie_data = f.readlines()
 f.close()
 
 # data about previously watched movie
-watched_movie_name = "Planet Hulk"
-watched_movie_description = "Will he save their world or destroy it? When the Hulk becomes too dangerous for the Earth, the Illuminati trick Hulk into a shuttle and launch him into space to a planet where the Hulk can live in peace. Unfortunately, Hulk land on the planet Sakaar where he is sold into slavery and trained as a gladiator."
-watched_movie = f"{watched_movie_name} :{watched_movie_description}"
+watched_movie = "Planet Hulk :Will he save their world or destroy it? When the Hulk becomes too dangerous for the Earth, the Illuminati trick Hulk into a shuttle and launch him into space to a planet where the Hulk can live in peace. Unfortunately, Hulk land on the planet Sakaar where he is sold into slavery and trained as a gladiator."
+watched_movie_name = watched_movie.split(" :")[0]
+watched_movie_description = watched_movie.split(" :")[1]
 
 # final output
 print(f"\nSince you've watched {watched_movie_name}, why not try this:\n")
